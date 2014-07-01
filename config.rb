43605c56b239b11da65934f840960eb42fd7584f
Gil Barbara <gilbarbara@gmail.com>
2014-06-30 21:01:37 -0300

    first version

--- config.rb
+++ config.rb
@@ -3,15 +3,19 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "/"
-sass_dir = "/"
+sass_dir = "styles"
 images_dir = ""
 javascripts_dir = ""
+
+output_style = (environment == :production) ? :compressed : :expanded
+
+add_import_path 'bower_components'
 
 # You can select your preferred output style here (can be overridden via the command line):
  output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
